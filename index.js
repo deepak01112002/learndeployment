e3996f7f3b576eccbd55b3d1efe8107e930ddd69
Deepak Pandey <deepak8574757609@gmail.com>
2023-04-30 15:24:14 +0530

    full bakend deployment

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const connection = require("./db")
 const UserRoute = require("./Routes/user.route")
 const NoteRoute = require("./Routes/Notes.route")
 const cors = require("cors")
+require("dotenv").config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-
-app.get("/",(req,res)=>{
-    res.send("Homepage")
-})
 app.use("/user",UserRoute)
 app.use("/note",NoteRoute)
 
-app.listen(8080,async()=>{
+app.listen(process.env.port,async()=>{
     try {
         await connection
         console.log("Connection done at port")
